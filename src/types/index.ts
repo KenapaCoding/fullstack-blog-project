bc19ac751f610d6ffecfd5faafc9af1d63bec0b8
ranggo pato <patoranggo@gmail.com>
2024-07-25 12:57:54 +0700

    blogsite project finish

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export interface Blogs {
+    title: string,
+    description:string,
+    imageURL:string,
+}
+export interface DTOBlogs {
+    _id: string
+    title: string,
+    description:string,
+    imageURL:string,
+    createdAt:string,
+    updatedAt:string,
+    __v:number
+}
